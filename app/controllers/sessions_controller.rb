72418d7dd62c75568673db8bc1e5b1ae44756bb2
chapchon819 <l000merci.819@gmail.com>
2024-02-20 16:21:22 +0000

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path, status: :see_other
   end
 end
